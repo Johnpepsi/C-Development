a007672483778c4c9f02321951f389a0cd6e289f
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-09-09 01:37:34 -0400

    Add files via upload

---
+++ Repos/Windows Form/First Project/firstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hey Mr. " + name + " Welcome to the league of Legends! \n");
+
+        Console.WriteLine("Enter your age: ");
+        // Read the age as a string and then parse it to an integer
+        string ageBakri = Console.ReadLine();
+
+        Console.WriteLine("So you are " + ageBakri + " Nice Job buddy");
+       
+    }
+}

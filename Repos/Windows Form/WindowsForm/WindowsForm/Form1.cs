6a6e973b4a715491d18e6bbc1c2a12cf61d0af42
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-09-09 01:35:07 -0400

    Add files via upload

---
+++ Repos/Windows Form/WindowsForm/WindowsForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string message = "Did you see the icon? ";
+            string title = "ok";
+            MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        
+        }
+    }
+}
